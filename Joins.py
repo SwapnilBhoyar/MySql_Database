dc4ff026e5799585bc4ec33a88405d5040639330
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-07-21 00:33:49 +0530

    [swapnil] Refactor: right join

--- Joins.py
+++ Joins.py
@@ -78,10 +78,23 @@ class Joins:
         except Exception as e:
             Log.logging.error(e)
 
+    
+    def rightJoin(self):
+        try:
+            sql = "SELECT student_info.exam_no,student_info.date_of_birth,student.student_name FROM student_info RIGHT JOIN student ON student_info.roll_no=student.roll_no;"
+            self.mycursor.execute(sql)
+            myresult = self.mycursor.fetchall()
+            for x in myresult:
+                Log.logging.info(x)
+        except Exception as e:
+            Log.logging.error(e)
+
+
 if __name__=="__main__":
     join = Joins()
     # join.createTable()
     # join.insertMultipleData()
     # join.printData()
     # join.innerJoin()
-    join.leftJoin()
+    # join.leftJoin()
+    join.rightJoin()
